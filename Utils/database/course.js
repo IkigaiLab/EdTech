3aede1aa2540b652ed92fe325bd8603b383e666e
IkigaiLab <pratik.agarwal@theikigailab.com>
2021-10-25 11:23:29 +0530

    Added Database folder structure

---
+++ Utils/database/course.js
@@ -0,0 +1,17 @@
+function createCourse(data) {
+  // Adding data in firebase
+}
+
+function deleteCourse(data) {
+  // Delete course from database
+}
+
+function getAllCourse(data) {
+  // Return all the courses stored in database to the user
+}
+
+module.exports = {
+  createCourse,
+  deleteCourse,
+  getAllCourse,
+};
